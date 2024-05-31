0265071d73abdb4e04c43e268b7a3a945d529b92
sptres <shawnpark2397@gmail.com>
2024-05-30 22:56:27 -0700

    text change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,18 +6,18 @@ const Hero = () => {
     <div className="bg-emerald-100 py-24">
       <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
-          <h1 className="text-7xl font-bold tracking-wider">I'm Shawn</h1>
+          <h1 className="text-7xl font-bold tracking-wider">Shawn Park</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
             Software Developer
           </p>
           <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide">
-            turning ideas into interactive reality
+            making life easier one step at a time
           </p>
           <div className="flex gap-x-4 mt-4">
-            <a href="#">
+            <a href="https://github.com/sptres">
               <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com/in/seunghyukpark/">
               <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
             <a href="#">
