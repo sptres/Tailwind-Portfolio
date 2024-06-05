45b658ba0e9aefb467591057e6bfe3b73aaf87d5
sptres <shawnpark2397@gmail.com>
2024-06-04 23:07:43 -0700

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,13 @@ const About = () => {
         <article>
           <SectionTitle text="design and build" />
           <p className="text-slate-600 mt-8 leading-loose">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            aliquid itaque vitae magni deserunt a nam, distinctio odit,
-            nesciunt, qui ullam aperiam molestiae. Voluptatem pariatur libero
-            veniam nisi maxime fugiat.
+            With a strong foundation in HTML, CSS, and JavaScript, I create
+            engaging and responsive web designs. My expertise in TypeScript and
+            React allows me to build dynamic, component-based front-end
+            applications, while my skills in Node.js ensure robust back-end
+            solutions. I leverage Python for efficient data manipulation and
+            algorithms, and manage databases effectively with MongoDB and
+            PostgreSQL to deliver comprehensive web solutions.
           </p>
         </article>
       </div>
